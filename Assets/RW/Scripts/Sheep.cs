d7266e7936349300d7232a60c1c229ad373a4203
jordialfonsop <jordialfonsop@gmail.com>
2022-04-25 18:39:00 +0200

    Completed part 1

---
+++ Assets/RW/Scripts/Sheep.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sheep : MonoBehaviour
+{
+
+    public float runSpeed;
+    public float gotHayDestroyDelay;
+    private bool hitByHay;
+    public float dropDestroyDelay;
+    private Collider myCollider;
+    private Rigidbody myRigidbody;
+    private SheepSpawner sheepSpawner;
+
+    public void SetSpawner(SheepSpawner spawner)
+    {
+        sheepSpawner = spawner;
+    }
+
+    void Start(){
+        myCollider = GetComponent<Collider>();
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * runSpeed * Time.deltaTime);
+    }
+
+    private void HitByHay()
+    {
+        sheepSpawner.RemoveSheepFromList (gameObject);
+        hitByHay = true; // 1
+        runSpeed = 0; // 2
+        Destroy(gameObject, gotHayDestroyDelay); // 3
+    }
+
+    private void Drop()
+    {
+        sheepSpawner.RemoveSheepFromList (gameObject);
+        myRigidbody .isKinematic = false;
+        myCollider .isTrigger = false;
+        Destroy(gameObject, dropDestroyDelay );
+    }
+
+    private void OnTriggerEnter (Collider other) // 1
+    {
+        if (other.CompareTag("Hay") && !hitByHay) // 2
+        {
+            Destroy(other.gameObject); // 3
+            HitByHay(); // 4
+        }
+        else if (other.CompareTag("DropSheep"))
+        {
+            Drop();
+        }
+    }
+
+
+
+}
---
+++ Assets/RW/Scripts/hayMachineMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hayMachineMovement : MonoBehaviour
+{
+
+    public float movementSpeed;
+    public float horizontalBoundary = 22;
+    public GameObject hayBalePrefab;
+    public Transform haySpawnpoint;
+    public float shootInterval;
+    private float shootTimer;
+
+    private void UpdateMovement()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal"); // 1
+        if (horizontalInput < 0 && transform.position.x > -horizontalBoundary) // 1
+        {
+            transform.Translate(transform.right * -movementSpeed * Time.deltaTime);
+        }
+        else if (horizontalInput > 0 && transform.position.x < horizontalBoundary) // 2
+        {
+            transform.Translate(transform.right * movementSpeed * Time.deltaTime);
+        }
+    }
+
+    private void ShootHay()
+    {
+        Instantiate(hayBalePrefab, haySpawnpoint .position, Quaternion.identity);
+    }
+
+    private void UpdateShooting() 
+    {
+        shootTimer -= Time.deltaTime;
+        if (shootTimer <= 0 && Input.GetKey(KeyCode.Space)) {
+            shootTimer = shootInterval;
+            ShootHay();
+        }
+    }  
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMovement();
+        UpdateShooting();
+    }
+}
